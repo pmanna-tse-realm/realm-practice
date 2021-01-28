4e4fddea59d52bbb2c65c863060480bc5fd1c703
Paolo <paolo.manna@mongodb.com>
2021-01-28 10:38:36 +0000

    Initial Commit

---
+++ RealmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmPractice
+//
+//  Created by Paolo Manna on 28/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
